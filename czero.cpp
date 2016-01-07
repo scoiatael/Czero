03adcffb0cb037e7173dbc51db86cf060a32dff9
scoiatael <czapl.luk@gmail.com>
2016-01-07 23:08:43 +0000

    init

---
+++ czero.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "parser.h"
+
+int main() {
+  std::cout << "Hello world!" << std::endl;
+}
---
+++ parser.m
@@ -0,0 +1,17 @@
+%token IDENTIFIER
+%token PLUS_SIGN STAR_SIGN LEFT_PARANTHESES RIGHT_PARANTHESES
+%startsymbol S EOF
+
+
+% S : S PLUS_SIGN T
+%   | T
+%   ;
+
+
+% T : T STAR_SIGN U
+%   | U
+%   ;
+
+% U : IDENTIFIER
+%   | LEFT_PARANTHESES S RIGHT_PARANTHESES
+%   ;
