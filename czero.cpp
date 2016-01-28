2421cd9958077cf7566034aba1d762b871d13af2
scoiatael <czapl.luk@gmail.com>
2016-01-28 15:31:35 +0100

    Remove junk from main

--- czero.cpp
+++ czero.cpp
@@ -1,30 +1,6 @@
 #include "czero.h"
 #include "assert.h"
-#include "parser/varstore.h" // damnit
-#include "parser/reader.h"
-#include "parser/tokenizer.h"
-#include "parser/parser.h"
 
 int main(int argc, char *argv[]) {
-  tokenizer tt;
-
-#ifdef TEST_TOKENIZER
-  tt.scan();
-
-  while(tt.lookahead.size() && tt.lookahead.front().type != tkn_EOF)
-  {
-      std::cout << tt.lookahead.front() << std::endl;
-      tt.lookahead.pop_front();
-      tt.scan();
-  }
-
-  ASSERT(tt.lookahead.size());
-  std::cout << tt.lookahead.front() << std::endl;
-#endif
-
-  varstore vs;
-  parser(tt, vs, tkn_PROG, 10);
-  ASSERT(tt.lookahead.size());
-
   return 0;
 }
