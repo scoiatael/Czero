a0643bcc48a9f5b85b23d69d97017595b0f011ea
scoiatael <czapl.luk@gmail.com>
2016-02-06 16:42:42 +0100

    Main function with some more complex logic

--- czero.cpp
+++ czero.cpp
@@ -5,8 +5,8 @@ int main(int argc, char *argv[]) {
   ast::Program program;
 
   // Create printf prototype
-  std::string printf_function("printf");
   auto printf = std::make_shared<ast::Extern>();
+  std::string printf_function("printf");
   printf->identifier = printf_function;
   printf->return_value = ast::types::Int32;
   printf->variadic = true;
@@ -14,46 +14,51 @@ int main(int argc, char *argv[]) {
   program.body.push_back(printf);
 
   // Create main prototype
+  auto i_var = ast::Variable("i", ast::types::Int32);
   auto main = std::make_shared<ast::Func>();
   main->identifier = "main";
   main->return_value = ast::types::Int32;
+  main->local_variables.push_back(i_var);
   program.body.push_back(main);
 
   // Call printf(hello_world)
-  auto printf_context = std::make_shared<ast::VoidContext>();
-  main->body.push_back(printf_context);
-  printf_context->operation = std::make_shared<ast::Call>();
-  auto printf_call = dynamic_cast<ast::Call*>(printf_context->operation.get());
+  auto printf_call = std::make_shared<ast::Call>();
   printf_call->function_name = printf_function;
-  auto hello_world_constant = std::make_shared<ast::Constant>();
-  printf_call->arguments.push_back(hello_world_constant);
-  hello_world_constant->type = ast::types::String;
-  auto hello_world_constant_value = std::make_shared<ast::types::StringValue>();
-  hello_world_constant->value = hello_world_constant_value;
-  hello_world_constant_value->value = "Hello world!\n";
+  printf_call->arguments.push_back(std::make_shared<ast::Constant>
+                                   (std::make_shared<ast::types::StringValue>
+                                    ("Hello world!\n")));
 
-  // while(true) { printf("Hello world\n") }
+  // while(i < 3) { printf("Hello world\n"); i--; }
   auto whil = std::make_shared<ast::While>();
+  auto cond = std::make_shared<ast::BinOp>();
+  cond->operator_ = "lt";
+  auto cond_lhs = std::make_shared<ast::Variable>(i_var);
+  auto cond_rhs = std::make_shared<ast::Constant>(std::make_shared<ast::types::IntValue>(3));
+  cond->left = cond_lhs;
+  cond->right = cond_rhs;
+  cond->type = ast::types::Bool;
+  whil->condition = cond;
+  whil->body.push_back(std::make_shared<ast::VoidContext>(printf_call));
+
+  auto decr = std::make_shared<ast::BinOp>();
+  decr->operator_ = "add";
+  auto decr_lhs = std::make_shared<ast::Variable>(i_var);
+  auto decr_rhs = std::make_shared<ast::Constant>(std::make_shared<ast::types::IntValue>(1));
+  decr->left = decr_lhs;
+  decr->right = decr_rhs;
+  decr->type = ast::types::Int32;
+  auto assign = std::make_shared<ast::Assignment>();
+  assign->identifier = i_var.identifier;
+  assign->value = decr;
+  whil->body.push_back(assign);
+
   main->body.push_back(whil);
-  whil->condition = std::make_shared<ast::Constant>();
-  auto while_cond_const =
-    dynamic_cast<ast::Constant*>(whil->condition.get());
-  while_cond_const->type = ast::types::Bool;
-  while_cond_const->value = std::make_shared<ast::types::BoolValue>();
-  auto whil_cond_cons_value =
-    dynamic_cast<ast::types::BoolValue*>(while_cond_const->value.get());
-  hello_world_constant_value->value = true;
-  whil->body.push_back(std::make_shared<ast::VoidContext>(*printf_context));
 
   // Return 0 for good measure
   auto main_return = std::make_shared<ast::Return>();
+  main_return->value =
+    std::make_shared<ast::Constant>(std::make_shared<ast::types::IntValue>(0));
   main->body.push_back(main_return);
-  main_return->value = std::make_shared<ast::Constant>();
-  auto main_return_value = dynamic_cast<ast::Constant*>(main_return->value.get());
-  main_return_value->type = ast::types::Int32;
-  main_return_value->value = std::make_shared<ast::types::IntValue>();
-  auto main_return_value_value = dynamic_cast<ast::types::IntValue*>(main_return_value->value.get());
-  main_return_value_value->value = 0;
 
   std::cout << program;
   auto ir = ir_generator::Context("czero_main");
